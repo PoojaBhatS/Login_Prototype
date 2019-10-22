d21ab28bb7d2ee48439abada1d83931e22807e5d
PoojaBhatS <pooja.santhapadavu@stv.tv>
2019-10-22 16:53:13 +0100

    Initial Commit

---
+++ Login_aws/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login_aws
+//
+//  Created by Pooja Santhapadavu on 22/10/2019.
+//  Copyright © 2019 Pooja Santhapadavu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+override func viewDidLoad() {
+super.viewDidLoad()
+// Do any additional setup after loading the view.
+}
+
+
+}
+
